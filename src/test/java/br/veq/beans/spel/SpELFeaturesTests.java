e03cfe6c8e278fad8fbb8467aef97c3f7cf68f83
Guilherme de Castro <guilycst@gmail.com>
2018-03-26 00:22:24 -0300

    Spring expression language tests

--- src/test/java/br/veq/beans/spel/SpELFeaturesTests.java
+++ src/test/java/br/veq/beans/spel/SpELFeaturesTests.java
@@ -2,6 +2,7 @@ package br.veq.beans.spel;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.Test;
 
@@ -16,6 +17,40 @@ public class SpELFeaturesTests extends SpringTests {
 		assertEquals(5.5, bean.getFloatNumber());
 		assertEquals("Hello", bean.getaString());
 		assertTrue(bean.isaBoolean());
+	}
+
+	@Test
+	public void testMathOperators() {
+		SpELMathOperators bean = context.getBean(SpELMathOperators.class);
+		assertEquals(10, bean.getSum());
+		assertEquals(1, bean.getSubs());
+		assertEquals(25, bean.getMult());
+		assertEquals(1, bean.getDiv());
+		assertEquals(1, bean.getMod());
+		assertEquals(9, bean.getPow());
+	}
+
+	@Test
+	public void testLogicalOperators() {
+		SpELLogicalOperators bean = context.getBean(SpELLogicalOperators.class);
+		assertTrue(bean.isGreaterThan());
+		assertTrue(bean.isEquality());
+		assertTrue(bean.isLesserThan());
+		assertTrue(bean.isNot());
+		assertTrue(bean.isNotEqual());
+		assertTrue(bean.isGreaterOrEqThan());
+		assertTrue(bean.isLesserOrEqThan());
+		assertEquals("one is equals to one", bean.getTernary());
+	}
+
+	@Test
+	public void testReferences() {
+		System.setProperty("xpto.foo", "Hola");
+		SpELReferences bean = context.getBean(SpELReferences.class);
+		assertNotNull(bean.getLogicalOperatorsRef());
+		assertEquals(10, bean.getReferencedValue());
+		assertEquals(10, bean.getReferencedMethodCallValue());
+		assertEquals("Hola", bean.getSysProp());
 	}
 
 }
